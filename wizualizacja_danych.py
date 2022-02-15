f9933cf38229b294c6c34353ceddea6937383896
Tomasz.Lipka <tlipka17@gmail.com>
2022-02-15 21:02:07 +0100

    added after 5 class

---
+++ wizualizacja_danych.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+# plt.style.use('ggplot')
+# for i in range(4):
+#     plt.plot(np.random.rand(10))
+# plt.show()
+# x = np.linspace(0, 10, 100)
+#
+# fig = plt.figure()
+# plt.plot(x, np.sin(x), '-')
+# plt.plot(x, np.cos(x), '--');
+# plt.show()
+# fig.savefig('my_figure.png') #zapis wykresu/obrazka
+
+from IPython.display import Image
+Image('my_figure.png')
